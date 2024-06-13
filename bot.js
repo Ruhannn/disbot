2da2b3d604dc5e0f5b4d96a3117ee9b91f04d39f
Ruhannn <kamiayaka09@gmail.com>
2024-06-14 03:04:07 +0600

    cors added

--- bot.js
+++ bot.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const cors = require('cors');
 const { Client, GatewayIntentBits } = require("discord.js");
 const keep_alive = require("./keep_alive").keepAlive;
 require("dotenv").config();
 const app = express();
+
+app.use(cors({ origin: "*" }));
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "*"],
     methods: ["GET", "POST"],
   },
 });
